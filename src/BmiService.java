a2bff6f74c6f8da47f54c8b44d0ee8110353e5a9
Elena Vinogradova <elena20031999@gmail.com>
2023-04-07 16:02:47 +0200

    update

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate (int weightKg, double heightM) {
-        double index = weightKg / (heightM * heightM);
+        double index = weightKg / Math.pow(heightM, 2);
         int bmi = (int) index;
         return bmi;
     }
